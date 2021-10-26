0b47daf718aed10116df7f1ce3b80edfef4ac0b2
tabatha wiggers <tatisabato@hotmail.com>
2021-10-26 20:30:06 -0300

    deletes bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,6 +17,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
   private
 
   def bookmark_params
